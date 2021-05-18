1b15bd67f6ecee67aa5e0f122409325ca99f473f
Seton <nighthawk1304@gmail.com>
2021-05-19 10:35:49 +1200

    first class

---
+++ Class1.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Class1 here.
+ *
+ * @author Seton Spence
+ * @version 0.1
+ */
+public class Class1
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    public Class1()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+}
