e70b4fe80afd286a9328ad9dbb01ffa4bdcdf92b
Seton <nighthawk1304@gmail.com>
2021-05-30 11:26:01 +1200

    added imput and placeholders as well as startup proses and splash screen

--- Class1.java
+++ Class1.java
@@ -1,4 +1,3 @@
-
 /**
  *
  * @author Seton Spence
@@ -13,11 +12,56 @@ public class Class1
 {
     // instance variables - replace the example below with your own
     private int x;
+    
+    Scanner scanner = new Scanner(System.in);//create a scanner
+
 
     public Class1()
     {
         // initialise instance variables
-        x = 0;
+        startUp();
+        imput();
     }
-
+    
+    void startUp(){
+        System.out.println("******");
+        
+        System.out.println("┏━━━┓━━━━━━━━━┏┓━━━━━┏━━━┓━━━━━━━━━┏┓━━━━━━━━┏┓━━━━━┏━━━┓━━━━━━━━━━━━━━━━━━━━━━━");
+        System.out.println("┃┏━┓┃━━━━━━━━┏┛┗┓━━━━┃┏━┓┃━━━━━━━━┏┛┗┓━━━━━━━┃┃━━━━━┃┏━┓┃━━━━━━━━━━━━━━━━━━━━━━━");
+        System.out.println("┃┗━┛┃┏━━┓┏━━┓┗┓┏┛━━━━┃┃━┗┛┏━━┓┏━┓━┗┓┏┛┏━┓┏━━┓┃┃━━━━━┃┗━┛┃┏━┓┏━━┓┏━━┓┏━┓┏━━┓━┏┓┏┓");
+        System.out.println("┃┏━━┛┃┏┓┃┃━━┫━┃┃━━━━━┃┃━┏┓┃┏┓┃┃┏┓┓━┃┃━┃┏┛┃┏┓┃┃┃━━━━━┃┏━━┛┃┏┛┃┏┓┃┃┏┓┃┃┏┛┗━┓┃━┃┗┛┃");
+        System.out.println("┃┃━━━┃┃━┫┣━━┃━┃┗┓━━━━┃┗━┛┃┃┗┛┃┃┃┃┃━┃┗┓┃┃━┃┗┛┃┃┗┓━━━━┃┃━━━┃┃━┃┗┛┃┃┗┛┃┃┃━┃┗┛┗┓┃┃┃┃");
+        System.out.println("┗┛━━━┗━━┛┗━━┛━┗━┛━━━━┗━━━┛┗━━┛┗┛┗┛━┗━┛┗┛━┗━━┛┗━┛━━━━┗┛━━━┗┛━┗━━┛┗━┓┃┗┛━┗━━━┛┗┻┻┛");
+        System.out.println("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┏━┛┃━━━━━━━━━━━━");
+        System.out.println("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┗━━┛━━━━━━━━━━━━");
+        
+        System.out.println("type help for help");
+        //System.out.println("******");
+    }
+    
+    boolean running = true;
+    void imput(){
+        String Command;
+        while(running == true){//the while loop that deals with imput
+            Command = scanner.nextLine();//each time the loop runs thrugh set command as the line enterd next. this givs a neer instant responce when a command in enterd
+            Command = Command.toLowerCase();//set the string to lower case to deal with captlisation e.g. FoWaRD --> foward 
+            switch (Command){
+                case "pest" :
+                    
+                break;
+                
+                case "help" :
+                    System.out.println("here is a list of commands:");
+                    System.out.println("command place holder 1");
+                    System.out.println("command place holder 2");
+                    System.out.println("command place holder 3");
+                break;
+                
+                case "end" :
+                System.out.println("program terminated");
+                running = false;
+                break;
+            }
+        }   
+    }
 }
