5a776b618c6ba216b6dbebd2571313b716a6c05a
Seton <nighthawk1304@gmail.com>
2021-05-30 10:28:04 +1200

    added dependencies

--- Class1.java
+++ Class1.java
@@ -1,10 +1,14 @@
 
 /**
- * Write a description of class Class1 here.
  *
  * @author Seton Spence
  * @version 0.1
  */
+
+import java.util.Scanner;   //Scanner for keybord imput and file reading
+import java.io.File;        //for interacting with files
+import java.io.IOException; //for handling errors
+
 public class Class1
 {
     // instance variables - replace the example below with your own
