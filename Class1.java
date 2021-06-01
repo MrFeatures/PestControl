b5794c559309a17855a30246cfe0ef99ebda01ae
Seton <nighthawk1304@gmail.com>
2021-06-02 08:44:39 +1200

    test

--- Class1.java
+++ Class1.java
@@ -5,6 +5,14 @@
  * @author Seton Spence
  * @version 0.1
  */
+
+import java.util.Scanner;   //Scanner for keybord imput and file reading
+import java.util.Arrays;
+
+import java.io.File;        //file handles
+import java.io.IOException; //handle errors
+
+
 public class Class1
 {
     // instance variables - replace the example below with your own
